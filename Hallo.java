884486aa8a5bd18b1c972f9f4ce415909fde198e
Guenter-Hofer <guenter-hofer@aon.at>
2014-06-01 22:38:47 +0200

    erstes Commit

---
+++ Hallo.java
@@ -0,0 +1,7 @@
+public class Hallo { // Hallo ist der Klassenname, der mit dem Dateinamen
+                     // ohne Suffix übereinstimmen muss
+  public static void main(String[] args){// main muss main heißen
+    System.out.println("Hallo, Welt!");
+  }
+}
+
